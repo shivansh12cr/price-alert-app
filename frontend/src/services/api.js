8c34dc165a3b498852fcfc58ccab7ee8dada3bc3
shivansh gupta <2022uec1637@mnit.ac.in>
2026-06-06 02:24:36 +0530

    add backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api"
+    baseURL: "https://price-alert-app-t3br.onrender.com/"
 });
 
 export default api;
--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://price-alert-app-t3br.onrender.com/");
 
 export default socket;
