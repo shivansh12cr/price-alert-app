7bf5d25e07cde36e6544abaea5e755116aa61904
shivansh gupta <2022uec1637@mnit.ac.in>
2026-06-06 11:25:03 +0530

    fix cryptocompare api1

--- backend/src/services/marketService.js
+++ backend/src/services/marketService.js
@@ -2,32 +2,16 @@ const axios = require("axios");
 
 const getCryptoPrice = async (symbol) => {
 
-    const map = {
-        BTCUSDT: "BTC",
-        ETHUSDT: "ETH",
-        SOLUSDT: "SOL",
-        BNBUSDT: "BNB",
-        XRPUSDT: "XRP",
-        DOGEUSDT: "DOGE"
-    };
-
-    const coin = map[symbol.toUpperCase()];
-
-    if (!coin) {
-        throw new Error(
-            `Unsupported crypto symbol: ${symbol}`
-        );
-    }
-
     const response =
         await axios.get(
-            `https://min-api.cryptocompare.com/data/price?fsym=${coin}&tsyms=USD`
+            `https://api.twelvedata.com/price?symbol=${symbol}&apikey=${process.env.TWELVE_DATA_API_KEY}`
         );
 
     return Number(
-        response.data.USD
+        response.data.price
     );
 };
+
 
 const getGoldPrice = async () => {
 
